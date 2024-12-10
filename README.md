c78068ee49d6442d2e5045e415fdcb072df1c219
Izabela <izabela.m.alfenas@gmail.com>
2024-12-10 17:59:10 -0300

    initial server setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-A simple file upload feature using nothing but node + ts
+A simple file upload feature using nothing but node + ts (and ts-node to run it 😆).
+
+The first version will have almost no validations bc I want it to be vulnerable. I will pentest it and then I will implement the security requirements to make it more reliable
---
+++ backend/index.ts
@@ -0,0 +1,17 @@
+import { createServer } from 'http';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const server = createServer();
+
+const htmlPath = join(__dirname, '..', 'client', 'index.html')
+
+server.on('listening', () => console.log('Listening.'));
+server.on('request', (request, response) => {
+  if(request.url === '/') {
+    response.end(readFileSync(htmlPath));
+    return
+  }
+});
+
+server.listen(3333);
